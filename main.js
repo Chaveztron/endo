94e81689e89ebe08f42629ad13e89b60d22f919d
Jesus Chavez <jesus.chavez.caro@uabc.edu.mx>
2020-07-27 08:47:57 -0700

    Integracion de Knex y base de datos a la aplicacion

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
-const electron = require('electron');
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
+const { app, BrowserWindow, ipcMain } = require('electron')
 
 const isDev = require('electron-is-dev');
 
 let mainWindow;
 
+  const options = {
+    client: 'sqlite3',
+    connection: {
+      filename: "./mydb.sqlite"
+    },
+    useNullAsDefault: true
+    }
+
 const createWindow = () => {
     mainWindow = new BrowserWindow({
         width: 1280,
         height: 720,
-        webPreferences: { nodeIntegration: true }
+        webPreferences: { 
+            nodeIntegration: true
+        }
     });
 
     mainWindow.setMenu(null)
@@ -21,14 +29,55 @@ const createWindow = () => {
         mainWindow.loadFile('build/index.html')
     }
 
-    if (isDev) {
+    
         mainWindow.webContents.openDevTools();
-    }
+    
 
     mainWindow.on('closed', () => mainWindow = null);
 }
 
 app.on('ready', createWindow);
+
+app.on('ready', () => {
+    
+    ipcMain.on('add-paciente', (event, arg) => {
+        const knex = require('knex')(options);
+        const name = arg.nombre +' '+ arg.dato
+        knex('paciente').insert({ 
+            nombre: arg.nombre,
+            apellido_paterno: arg.apellido_paterno,
+            apellido_materno: arg.apellido_materno,
+            genero: arg.genero,
+            nacimiento: arg.nacimiento,
+            telefono: arg.telefono,
+            sexo: arg.sexo
+        })
+        .catch((err) => { console.log(err); throw err })
+        .finally(() => {
+            knex.destroy();
+        });
+          
+        event.returnValue = 'dato insertado'
+      })
+    
+      
+      ipcMain.on('get-pacientes', (event, arg) => {
+        const knex = require('knex')(options);
+        var pacientes=[];
+        knex.from('paciente').select("*")
+        .then((rows) => {
+            for (row of rows) {
+                var paciente = {id: row.id, 'nombre': row.nombre };
+                pacientes.push(paciente);
+            }
+            event.returnValue = pacientes
+        }).catch((err) => { console.log( err); throw err })
+        .finally(() => {
+            knex.destroy();
+        });
+      })
+    
+  });
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
--- src/App.js
+++ src/App.js
@@ -1,12 +1,65 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Route, NavLink } from 'react-router-dom'
 import './App.css'
 import { Button } from "@blueprintjs/core";
 import { Form, Input, Select, NumInput, Radios, DatePiker } from "./components";
+const { ipcRenderer } = window.require("electron");
+
+const App = () => {
+  const [users, setUser] = useState(ipcRenderer.sendSync('get-pacientes'))
+
+  return (
+    <BrowserRouter>
+      <nav class="bp3-navbar .modifier">
+        <div class="bp3-navbar-group bp3-align-left">
+        <Route render={NavegacionImperativa} />
+        <span class="bp3-navbar-divider"></span>
+          <div class="bp3-navbar-heading">EndoClinic</div>
+          <input class="bp3-input" placeholder="Search files..." type="text" />
+        </div>
+        <div class="bp3-navbar-group bp3-align-right">
+        <NavLink to='/' exact activeClassName='active'><button class="bp3-button bp3-minimal bp3-intent-success bp3-icon-add">Agregar paciente</button></NavLink>
+        <NavLink to='/Pacientes' activeClassName='active'><button class="bp3-button bp3-minimal bp3-icon-database">Pacientes</button></NavLink>
+        <NavLink to='/videos' activeClassName='active'><button class="bp3-button bp3-minimal bp3-icon-document">Videos</button></NavLink>
+          <span class="bp3-navbar-divider"></span>
+          <button class="bp3-button bp3-minimal bp3-icon-user"></button>
+          <button class="bp3-button bp3-minimal bp3-icon-notifications"></button>
+          <button class="bp3-button bp3-minimal bp3-icon-cog"></button>
+        </div>
+      </nav>
+
+      <Route path='/' exact render={Add_paciente} />
+      <Route path='/Pacientes' render={Pacientes} />
+      <Route path='/videos' render={Videos} />
+
+      <ul>
+      {users.map(user =>(
+          <li key={user.id}>
+              { user.id }	{ user.nombre }
+          </li>
+      ))}
+      </ul>
 
 
-const add_paciente = () => {
-  const onSubmit = data => console.log(data);
+    </BrowserRouter>
+    
+  )
+}
+
+
+const Add_paciente = () => {
+  const onSubmit = data => {
+    console.log(data)
+    console.log(ipcRenderer.sendSync('add-paciente', {
+      nombre: data.Nombre,
+      apellido_paterno: data.Apellido_paterno,
+      apellido_materno: data.Apellido_materno,
+      genero: data.Genero,
+      nacimiento: data.Nacimiento,
+      telefono: data.Telefono,
+      sexo: data.Sexo
+    }))
+  };
   return(
     <>
       <h1>Agregar Paciente</h1>
@@ -15,16 +68,9 @@ const add_paciente = () => {
         <Input name="Apellido_paterno" />
         <Input name="Apellido_materno" />
         <NumInput name="Telefono"/>
-
-
         <Radios name="Sexo" values={["Woman", "Man"]}/>
-
         <Select name="Genero" options={["mujer", "hombre"]} />
-
         <DatePiker name="Nacimiento"/>
-
-        
-
         <Button type="submit" value="Submit" >Agregar </Button>
       </Form>
     </>
@@ -34,11 +80,14 @@ const add_paciente = () => {
 
 
 
-const pacientes = () => {
-  
+const Pacientes = (props) => {
   return(
+    <>
     <h1>Pacientes</h1>
+
+      </>
     )
+    
 }
 
 const Videos = () => (
@@ -56,33 +105,6 @@ const NavegacionImperativa = ({ history }) => {
   )
 }
 
-const App = () => {
 
-  return (
-    <BrowserRouter>
-      <nav class="bp3-navbar .modifier">
-        <div class="bp3-navbar-group bp3-align-left">
-        <Route render={NavegacionImperativa} />
-        <span class="bp3-navbar-divider"></span>
-          <div class="bp3-navbar-heading">EndoClinic</div>
-          <input class="bp3-input" placeholder="Search files..." type="text" />
-        </div>
-        <div class="bp3-navbar-group bp3-align-right">
-        <NavLink to='/' exact activeClassName='active'><button class="bp3-button bp3-minimal bp3-intent-success bp3-icon-add">Agregar paciente</button></NavLink>
-        <NavLink to='/pacientes' activeClassName='active'><button class="bp3-button bp3-minimal bp3-icon-database">Pacientes</button></NavLink>
-        <NavLink to='/videos' activeClassName='active'><button class="bp3-button bp3-minimal bp3-icon-document">Videos</button></NavLink>
-          <span class="bp3-navbar-divider"></span>
-          <button class="bp3-button bp3-minimal bp3-icon-user"></button>
-          <button class="bp3-button bp3-minimal bp3-icon-notifications"></button>
-          <button class="bp3-button bp3-minimal bp3-icon-cog"></button>
-        </div>
-      </nav>
-
-      <Route path='/' exact render={add_paciente} />
-      <Route path='/pacientes' render={pacientes} />
-      <Route path='/videos' render={Videos} />
-    </BrowserRouter>
-  )
-}
 
 export default App
